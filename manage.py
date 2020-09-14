cea79471307a6616e6192e5b27186b4c16916d33
y15034605069 <jhmyydd123123@qq.com>
2020-09-14 19:06:05 +0800

    版本v2

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,Response
 app = Flask(__name__)
 
 class Config(object):
@@ -9,6 +9,14 @@ app.config.from_object(Config)
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/add_user')
+def add_user():
+    return Response('........')
+
+@app.route('/user_list')
+def user_list():
+    return Response('........')
 
 if __name__ == '__main__':
     app.run()
