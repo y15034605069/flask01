8d9c5b201907a822a90c8e234e8a68b43b25b067
y15034605069 <jhmyydd123123@qq.com>
2020-09-14 19:03:15 +0800

    版本v1

---
+++ manage.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+class Config(object):
+    DEBUG = True
+
+app.config.from_object(Config)
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
